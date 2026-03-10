8ddc1e8d897ccd6534eec02727b8748f29ae0aa0
Eric Mbithi <mbithieri@gmail.com>
2026-03-10 12:00:52 +0300

    Rename the startGame function

---
+++ index.js
@@ -0,0 +1,40 @@
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+function startGame() {
+  renderGame()
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " +  firstCard + " " + secondCard
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw another Card?"
+  } else if (sum === 21) {
+    message = "Wohoo! You've got BlackJack!"
+    hasBlackJack = true
+  } else {
+    message = "You're Out of the Game!"
+    isAlive = false
+  }
+
+  messageEl.textContent = message
+
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck")
+  let card = 7
+  sum += card
+
+  startGame()
+}
+
