5b13fa574576d40eb7a8b2f3ad7f4e2d0e298934
Eric Mbithi <mbithieri@gmail.com>
2026-03-11 16:06:24 +0300

    Added randomNumberFunction

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -8,6 +8,21 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1
+  if (randomNumber > 10) {
+    return 10
+  } else if (randomNumber === 1) {
+    return 11
+  } else {
+    return randomNumber
+  }
+  
+}
+ 
+console.log(getRandomCard())
 
 
 function startGame() {
@@ -38,7 +53,7 @@ function renderGame() {
 function newCard() {
   console.log("Drawing a new card from the deck")
   
-  let card = 7
+  let card = getRandomCard();
   sum += card
   cards.push(card)
   renderGame()
