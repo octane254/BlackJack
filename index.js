a7f80a03063722d6e8131f2cb9ee2ffd29370757
octane254 <mbithieri@gmail.com>
2026-03-13 18:48:33 +0300

    Used an Object to Store Player Data

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+let player = {
+  name: "Eric",
+  chips: 145
+}
+
 let firstCard = getRandomCard();
 let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
@@ -9,6 +14,8 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1
@@ -64,3 +71,5 @@ function newCard() {
     renderGame()
   }
 }
+
+renderGame()
